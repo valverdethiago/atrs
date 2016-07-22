3a7571818c8fae745ec98b42496b8a106b04cc41
valverdethiago <valverde.thiago@gmail.com>
2016-07-22 11:16:32 -0300

    Faking mongo

--- atrs-service/src/main/java/com/crossover/atrs/respository/DatabaseConfiguration.java
+++ atrs-service/src/main/java/com/crossover/atrs/respository/DatabaseConfiguration.java
@@ -1,32 +1,22 @@
 package com.crossover.atrs.respository;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import com.mongodb.Mongo;
-import com.mongodb.MongoClient;
+import com.github.fakemongo.Fongo;
 
 @Configuration
 @EnableMongoRepositories(basePackages="com.crossover.atrs")
 @PropertySource(value="classpath:atrs.properties")
-public class DatabaseConfiguration extends AbstractMongoConfiguration{
+public class DatabaseConfiguration {
 
-    @Value("${database.host}")
-    private String hostname;
-    @Value("${database.name}")
-    private String name;
 
-	@Override
-	protected String getDatabaseName() {
-		return this.name;
-	}
-
-	@Override
-	public Mongo mongo() throws Exception {
-		return new MongoClient(this.hostname);
+	@Bean(name="mongoTemplate")
+	public MongoTemplate mongoTemplate() {
+		return new MongoTemplate(new Fongo("agilelink").getMongo(), "agilelink");
 	}
 
 
