a2133f5799ad92589cb096f74b53c98fb555854e
valverdethiago <valverde.thiago@gmail.com>
2016-07-22 18:31:12 -0300

    Rollback

---
+++ atrs-web/src/main/java/com/crossover/atrs/web/security/SecurityConfiguration.java
@@ -0,0 +1,43 @@
+package com.crossover.atrs.web.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Configuration class that holds useful beans and parameters to configure the
+ * authorization server part of the OAuth service
+ * 
+ * @author valverde.thiago
+ *
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.anyRequest().fullyAuthenticated()
+				.and()
+			.formLogin();
+	}
+
+	@Configuration
+	protected static class AuthenticationConfiguration extends
+			GlobalAuthenticationConfigurerAdapter {
+
+		@Override
+		public void init(AuthenticationManagerBuilder auth) throws Exception {
+			auth
+				.ldapAuthentication()
+					.userDnPatterns("uid={0},ou=people")
+					.groupSearchBase("ou=groups")
+					.contextSource().ldif("classpath:ldap.ldif");
+		}
+	}
+}
